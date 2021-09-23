fd9332039ed37ad19209394824159fbe3e84ae1c
Farhinarius <farhat3027@gmail.com>
2021-09-23 03:54:46 +0300

    boss health behaviour

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class HealthBar : MonoBehaviour
+    {
+        public static HealthBar Instance { get; private set; }
+
+        private Image _healthBarImage;
+
+        public float Value => _healthBarImage.fillAmount;
+
+        private void Awake() 
+        {
+            if (Instance == null) Instance = this;
+        }
+
+        private void Start() => _healthBarImage = transform.GetChild(1).GetComponent<Image>();
+
+        public void SetValue(float value) => _healthBarImage.fillAmount = value;
+
+    }
+}
