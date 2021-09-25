ebd3a920fc455e895c3e56348ae9d9e0cb42fa41
Farhinarius <farhat3027@gmail.com>
2021-09-25 20:07:25 +0300

    Item selection window

---
+++ Assets/Scripts/Items/Base/Movable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Items.Base
+{
+    public class Movable : MonoBehaviour
+    {
+        protected virtual void OnMouseDown()
+        {
+            
+        }
+
+        protected virtual void OnMouseUpAsButton()
+        {
+            
+        }
+    }
+}
